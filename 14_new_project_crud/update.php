3fbaf59fa5cc6a9120a115fe2501e1564dd5f8b0
Gulshan <gulshanbarhiya@gmail.com>
2021-05-30 15:33:34 +0530

    edit functionity fix

---
+++ 14_new_project_crud/update.php
@@ -0,0 +1,26 @@
+<?php
+ try {
+    $conn = new PDO('mysql:host=localhost;port=3306;dbname=products_db', 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+    if( $_SERVER['REQUEST_METHOD']==='POST') {
+        $prod_name=  $_POST['prod_name'];
+        $prod_type=  'SmartPhone';
+        $price=      $_POST['price'];
+        $description=$_POST['description'];
+
+        $stmt = $conn->prepare("UPDATE products SET prod_name = :product, prod_type= :type, price= :price, description= :description WHERE prod_id = :id");
+        $stmt->bindvalue(':product',$prod_name);
+        $stmt->bindvalue(':type',$prod_type);
+        $stmt->bindvalue(':price',$price);
+        $stmt->bindvalue(':description',$description);
+        $stmt->bindvalue(':id',1);
+        $edit=$stmt->execute();
+        if(isset($edit)){
+            echo "Successfully updated";
+        }
+    }
+?>
